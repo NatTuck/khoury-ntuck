11b324942c526e1f2db18628a5af251b9887df1f
Nat Tuck <nat@ferrus.net>
2016-11-28 15:25:32 -0500

    Fixes.

--- courses/2016/09/cs3650/notes/19-thread-examples/bank/bank.c
+++ courses/2016/09/cs3650/notes/19-thread-examples/bank/bank.c
@@ -1,6 +1,11 @@
 
+#include <sys/types.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
 // Note: intptr_t
 
@@ -36,7 +41,7 @@ transfer(Account* aa, Account* bb, long amount)
     }
 
     aa->balance -= amount;
-    aa->balance += amount;
+    bb->balance += amount;
 }
 
 void*
@@ -45,12 +50,14 @@ test_thread(void* seed_ptr)
     unsigned int seed = *((unsigned int*) seed_ptr);
     free(seed_ptr);
 
-    for (int kk = 0; kk < 100; ++kk) {
-        int ii = rand_r(&seed) % 4
+    for (int kk = 0; kk < 10000; ++kk) {
+        int ii = rand_r(&seed) % 4;
         int jj = rand_r(&seed) % 4;
         int aa = 1 + rand_r(&seed) % 100;
-        if (ii == jj)
+
+        if (ii == jj) {
             continue;
+        }
 
         transfer(accounts[ii], accounts[jj], aa);
     }
@@ -61,27 +68,31 @@ test_thread(void* seed_ptr)
 int
 main(int _ac, char* _av[]) 
 {
-    pthread_t threads[2];
+    pthread_t threads[10];
 
-    accounts[0] = make_account("Alice", 1000);
-    accounts[1] = make_account("Bob",   1000);
-    accounts[2] = make_account("Carol", 1000);
-    accounts[4] = make_account("Dave",  1000);
+    accounts[0] = make_account("Alice", 100000);
+    accounts[1] = make_account("Bob",   100000);
+    accounts[2] = make_account("Carol", 100000);
+    accounts[3] = make_account("Dave",  100000);
 
-    for (int ii = 0; ii < 2; ++ii) {
+    for (int ii = 0; ii < 10; ++ii) {
         unsigned int* seed = malloc(sizeof(unsigned int));
-        *seed = ii;
+        *seed = ii + getpid();
         pthread_create(&(threads[ii]), 0, test_thread, seed);
     }
 
-    for (int ii = 0; ii < 2; ++ii) {
+    for (int ii = 0; ii < 10; ++ii) {
         pthread_join(threads[ii], 0);
     }
 
+    long sum = 0;
     for (int ii = 0; ii < 4; ++ii) {
-        printf("%s: %ld\n", accounts[ii]->name, accounts[ii]->balance);
+        sum += accounts[ii]->balance;
+        printf("%s:\t%ld\n", accounts[ii]->name, accounts[ii]->balance);
         free_account(accounts[ii]);
     }
+
+    printf("\nSum:\t%ld\n", sum);
 
     return 0;
 }
--- courses/2016/09/cs3650/notes/19-thread-examples/notes.md
+++ courses/2016/09/cs3650/notes/19-thread-examples/notes.md
@@ -15,4 +15,10 @@ Bank Accounts
  - Identify data races; fix.
  - Identify deadlock issues; fix.
 
+## Condition Variable Example
 
+Input Buffer
+
+ - "Keyboard thread"
+ - "Display thread"
+ - Only display when buffer index > 0 && (index-1) == '\n'.
