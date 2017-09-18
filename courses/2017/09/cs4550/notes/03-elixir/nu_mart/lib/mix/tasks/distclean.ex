30405a1a16a988e56848e9a9f2d4ce5077b87b8e
Nat Tuck <nat@ferrus.net>
2017-09-17 22:35:58 -0400

    Update distclean.

--- courses/2017/09/cs4550/notes/03-elixir/nu_mart/lib/mix/tasks/distclean.ex
+++ courses/2017/09/cs4550/notes/03-elixir/nu_mart/lib/mix/tasks/distclean.ex
@@ -3,6 +3,6 @@ defmodule Mix.Tasks.Distclean do
 
   @shortdoc "Removes stuff we don't care about"
   def run(_) do
-    System.cmd("bash", ["-c", "rm -rf _build deps"])
+    System.cmd("bash", ["-c", "rm -rf _build deps assets/node_modules"])
   end
 end
