cd34da07b3fea2818a3d3e59e44e122ab6440ecf
Nat Tuck <nat@ferrus.net>
2017-11-09 10:02:35 -0500

    OTP notes

---
+++ courses/2017/09/cs4550/notes/17-hangman-2/player_channel_v1.ex
@@ -0,0 +1,30 @@
+defmodule HangmanWeb.PlayerChannel do
+  use HangmanWeb, :channel
+  alias Hangman.Game
+  alias Hangman.GameAgent
+
+  def join("player:" <> name, _payload, socket) do
+    if authorized?(socket, name) do
+      game = GameAgent.get(name) || Game.new()
+      GameAgent.put(name, game)
+      socket = socket
+      |> assign(:name, name)
+      {:ok, Game.client_view(game), socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  def handle_in("guess", %{"letter" => ll}, socket) do
+    name = socket.assigns[:name]
+    game = name
+    |> GameAgent.get()
+    |> Game.guess(ll)
+    GameAgent.put(name, game)
+    {:reply, {:ok, Game.client_view(game)}, socket}
+  end
+
+  defp authorized?(socket, name) do
+    socket.assigns[:username] == name
+  end
+end
